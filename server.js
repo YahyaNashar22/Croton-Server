5d8c2571df767806f8f3e0fd66996f23a51e5e0d
YahyaNashar22 <yahyanashar22@gmail.com>
2024-02-29 12:47:23 +0200

    cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 //CORS Policies
 app.use(cors({
-  origin:["http://localhost:3000","https://croton-client-aqk25ayvi-yahyanashar22s-projects.vercel.app"],
+  origin:["http://localhost:3000","https://croton-client-2g1bq6o7l-yahyanashar22s-projects.vercel.app"],
   credentials: true,
   optionsSuccessStatus: 200,}
 ));
